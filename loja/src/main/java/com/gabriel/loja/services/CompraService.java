6c3f3d8e563ec14b3ec3ef0b969e1d1367bb4644
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-06-26 17:13:41 -0300

    Adicionando RestTemplate na classe application e injetando no service para ler rota do Eureka

--- loja/src/main/java/com/gabriel/loja/services/CompraService.java
+++ loja/src/main/java/com/gabriel/loja/services/CompraService.java
@@ -1,7 +1,9 @@
 package com.gabriel.loja.services;
 
+import com.gabriel.loja.LojaApplication;
 import com.gabriel.loja.entities.dtos.CompraDTO;
 import com.gabriel.loja.entities.dtos.InfoFornecedorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,10 +11,12 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
+    @Autowired
+    private RestTemplate restTemplate;
     public void realizaCompra(CompraDTO compraDto) {
 
-        RestTemplate client = new RestTemplate();
-        ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://localhost:8081/fornecedores/info/" + compraDto.getEndereco().getEstado()
+//        RestTemplate client = new RestTemplate();
+        ResponseEntity<InfoFornecedorDTO> exchange = restTemplate.exchange("http://fornecedor/fornecedores/info/" + compraDto.getEndereco().getEstado()
                 , HttpMethod.GET
                 , null
                 , InfoFornecedorDTO.class);
