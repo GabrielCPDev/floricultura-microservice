c790e018defab7d7564c5d755a8604b786c7b3b1
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-17 12:26:19 -0300

    criando ProdutoRepository.java

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/repository/ProdutoRepository.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/repository/ProdutoRepository.java
@@ -2,11 +2,12 @@ package br.com.alura.microservice.fornecedor.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.alura.microservice.fornecedor.model.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Long>{
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
 	List<Produto> findByEstado(String estado);
 	
