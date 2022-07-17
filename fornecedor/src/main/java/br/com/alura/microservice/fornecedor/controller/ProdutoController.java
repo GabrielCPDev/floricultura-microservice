c0981193cfa9e1c8eed54b112655475f3919cb06
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-17 12:32:26 -0300

    refatorando ProdutoController.java

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/ProdutoController.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/ProdutoController.java
@@ -3,12 +3,14 @@ package br.com.alura.microservice.fornecedor.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.alura.microservice.fornecedor.model.Produto;
 import br.com.alura.microservice.fornecedor.service.ProdutoService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@RestController
 @RequestMapping("/produto")
 public class ProdutoController {
 
@@ -18,5 +20,13 @@ public class ProdutoController {
 	@RequestMapping("/{estado}")
 	public List<Produto> getProdutosPorEstado(@PathVariable("estado") String estado) {
 		return produtoService.getProdutosPorEstado(estado);
+	}
+	@PostMapping("/novo")
+	ResponseEntity<Produto> save(@RequestBody Produto produto){
+		Produto produtoSalvo = produtoService.save(produto);
+		var uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(produtoSalvo.getId()).toUri();
+		return ResponseEntity.created(uri).body(produtoSalvo);
 	}
 }
