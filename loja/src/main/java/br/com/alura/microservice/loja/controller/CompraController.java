9fe1a8a541707de9c16d200d93ec738f3046b3fe
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-17 12:34:47 -0300

    implementando o metodo que procura compra por id

--- loja/src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
+++ loja/src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
@@ -1,10 +1,7 @@
 package br.com.alura.microservice.loja.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.alura.microservice.loja.dto.CompraDTO;
 import br.com.alura.microservice.loja.model.Compra;
@@ -17,7 +14,12 @@ public class CompraController {
 	@Autowired
 	private CompraService compraService;
 
-	@RequestMapping(method = RequestMethod.POST)
+	@GetMapping("/{id}")
+	public Compra getById(@PathVariable Long id){
+		return compraService.getById(id);
+	}
+
+	@PostMapping
 	public Compra realizaCompra(@RequestBody CompraDTO compra) {
 		return compraService.realizaCompra(compra);
 	}
