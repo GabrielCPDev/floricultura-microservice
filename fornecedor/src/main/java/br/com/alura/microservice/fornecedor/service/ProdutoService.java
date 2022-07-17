a9a6303126522595a35d08da5510549439e4a02e
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-17 12:32:05 -0300

    adicionando injecao de dependencia e criando metodo que salva produto

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/ProdutoService.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/service/ProdutoService.java
@@ -2,6 +2,7 @@ package br.com.alura.microservice.fornecedor.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.microservice.fornecedor.model.Produto;
@@ -10,11 +11,16 @@ import br.com.alura.microservice.fornecedor.repository.ProdutoRepository;
 @Service
 public class ProdutoService {
 
+	@Autowired
 	private ProdutoRepository produtoRepository;
 	
 	public List<Produto> getProdutosPorEstado(String estado) {
 		return produtoRepository.findByEstado(estado);
 	}
 
-	
+
+	public Produto save(Produto produto) {
+		Produto save = produtoRepository.save(produto);
+		return save;
+	}
 }
