76840027c89069dd8ba4daa2a2fb9cb23711fabe
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-17 12:25:51 -0300

    mapeando entidade conta com jpa

--- loja/src/main/java/br/com/alura/microservice/loja/model/Compra.java
+++ loja/src/main/java/br/com/alura/microservice/loja/model/Compra.java
@@ -1,7 +1,15 @@
 package br.com.alura.microservice.loja.model;
 
-public class Compra {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 
+@Entity
+public class Compra implements Serializable {
+
+	@Id
 	private Long id;
 	
 	private Long pedidoId;
